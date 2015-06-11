c5f93742b63fd9d6f1cc5668c653de209e4a5400
lzf8989 <lzf8989@163.com>
2015-06-11 16:56:42 +0800

    comment

--- zz.py
+++ zz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+#通过scapy库抓取接口"eno2"网卡的入数据包，显示源IP，端口。
 import argparse
 from scapy.all import *
 pkts = sniff(iface="eno2", count=10000, prn=lambda x:x.sprintf("{IP:%IP.src% -> %IP.dst% %IP.proto% %IP.dport%}"))
